2597ed626308e13033a478be9aafb9c9f408d95c
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-23 16:01:46 -0700

    fixed npm run build errors

--- src/web/e/src/server/services/createUsers.ts
+++ src/web/e/src/server/services/createUsers.ts
@@ -11,15 +11,16 @@ const openai = new OpenAIApi(configuration);
 // generates users based on 3 lists of traits
 // returns bio's / ChatGPT system messages
 export default function generateUsers(mainTraits: string[], subTraits: string[], miniTraits: string[]): string[] {
-    let combinations: string[] = [];
+    const combinations: string[] = [];
     let newUser: string;
 
-    for (let a in mainTraits) {
-        let combination: (string | undefined)[] = [mainTraits[a]];
-        for (let b in subTraits) {
-            combination[1] = subTraits[b];
-            for (let c in miniTraits) {
-                combination[2] = subTraits[c];
+    for (const a of mainTraits) {
+        // const can be used because anything in mainTraits for loop scope is destroyed every iteration unless it is a global variable
+        const combination: (string | undefined)[] = [a];
+        for (const b of subTraits) {
+            combination[1] = b;
+            for (const c of miniTraits) {
+                combination[2] = c;
                 newUser = "You are a " + combination[0] + ". You also " + combination[1] + ". A fact about you is that you: " + combination[2];
                 combinations.push(newUser)
             }
