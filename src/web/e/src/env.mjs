e40a30294b554ae5599c5b199bc55fc7125dba3a
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-23 15:42:59 -0700

    fixed env.mjs

--- src/web/e/src/env.mjs
+++ src/web/e/src/env.mjs
@@ -46,6 +46,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
