bd1ff226818a36b28ee802a24be36276a9792c1d
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-24 13:04:29 -0700

    added askQuestion, got it working

---
+++ src/web/e/src/server/services/askQuestion.ts
@@ -0,0 +1,35 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration);
+
+
+export default async function askQuestion(user: string, question: string): Promise<boolean> {
+    // questions should be yes or no
+
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+            role: 'system',
+            content: user
+            }, 
+            {
+                role: 'user',
+                content: question + '. Answer yes or no without an explanation'
+            }]
+    }).then((result) => {
+        if (result.data.choices != undefined) {
+            return result.data.choices[0]?.message?.content
+        }
+    })
+    let result: boolean = false;
+    if (completion != undefined) {
+        if (completion.toLowerCase() == 'yes') {
+            return true;
+        }
+    }
+    return result;
+}
--- src/web/e/src/server/services/createUsers.ts
+++ src/web/e/src/server/services/createUsers.ts
@@ -1,12 +1,5 @@
-import { Configuration, OpenAIApi } from "openai";
-
 // string array type with a minimum of one string
 type arrayString = [string, ...string[]]
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(configuration);
 
 // generates users based on 3 lists of traits
 // returns bio's / ChatGPT system messages
