9cdc88f6789e4b94349fa7d7d08944657b8b02c0
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-25 16:28:14 -0700

    commented out trpc router until i can make sure the rest is working

--- src/web/e/src/server/api/routers/survey.ts
+++ src/web/e/src/server/api/routers/survey.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "e/server/api/trpc";
+import runSurvey from "e/server/services/runSurvey";
 
+/*
 const surveyRouter = createTRPCRouter({
     runSurvey: publicProcedure
-        .input(z.object({ mainTraits: z.array(z.string()), subTraits: z.array(z.string()), miniTraits: z.array(z.string()) }))
+        .input(z.object({ mainTraits: z.array(z.string()), subTraits: z.array(z.string()), miniTraits: z.array(z.string()), question: z.string() }))
         .query(({ input }) => {
-            
+            runSurvey(input.mainTraits, input.subTraits, input.miniTraits, input.question)
         })
 })
+*/
