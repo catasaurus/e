1c3ce5648cbaa42183e5ace2cbd91fde6490fab2
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-25 16:24:40 -0700

    added prisma create to runSurvey

--- src/web/e/src/server/services/runSurvey.ts
+++ src/web/e/src/server/services/runSurvey.ts
@@ -1,10 +1,37 @@
 import createUsers from "./createUsers";
 import askQuestion from "./askQuestion";
+import { PrismaClient } from "@prisma/client";
 
-export default async function runSurvey(mainTraits: string[], subTraits: string[], miniTraits: string[], question: string): Promise<void> {
+export default async function runSurvey(mainTraits: string[], subTraits: string[], miniTraits: string[], question: string, userId: string): Promise<void> {
+    const prisma = new PrismaClient()
     const users = createUsers(mainTraits, subTraits, miniTraits);
+    const userLength = users.length;
+
+    const newSurvey = await prisma.survey.create({
+        data: {
+            user: {
+                connect: { id: userId }
+            },
+            userCount: userLength,
+            question: question
+        },
+        select: {
+            id: true
+        }
+    })
 
     for (const user of users) {
-        const answer = askQuestion(user, question);
+        const answer = await askQuestion(user, question);
+
+
+        const newEntry = await prisma.surveyEntry.create({
+            data: {
+                user: user,
+                result: answer,
+                survey: {
+                    connect: { id: newSurvey.id }
+                }
+            }
+        })
     }
 }
