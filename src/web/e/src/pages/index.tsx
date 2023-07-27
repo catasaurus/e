cca5a33a21e93efe83fbe2f904ace8fcfce17397
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-27 14:49:04 -0700

    got mouse hover working

--- src/web/e/src/pages/index.tsx
+++ src/web/e/src/pages/index.tsx
@@ -2,9 +2,23 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "e/utils/api";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [isHovered, setIsHovered] = useState(false);
 
+  function onMouseEnterAuthButton(event: React.MouseEvent): void {
+    setIsHovered(true)
+  }
+
+  function onMouseLeaverAuthButton(event: React.MouseEvent): void{
+    setIsHovered(false)
+  }
+
+  let authButtonStyle = "flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg"
+  if (isHovered) {
+    authButtonStyle = "flex bg-gray-600 h-24 w-36 text-xl text-center rounded-lg"
+  }
   return (
     <div>
 
@@ -22,10 +36,10 @@ export default function Home() {
         </div>
 
         <div className="flex flex-auto justify-center flex-row space-x-28 relative inset-y-44">
-          <div className="flex bg-gray-600 h-14 w-32 text-l text-center rounded-lg">
+          <div className={authButtonStyle} onMouseEnter={onMouseEnterAuthButton} onMouseLeave={onMouseLeaverAuthButton}>
             <span className="m-auto">Get Started</span>
           </div>
-          <div className="flex bg-gray-600 h-14 w-32 text-l text-center rounded-lg">
+          <div className="flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg">
             <span className="m-auto">Sign In</span>
           </div>
         </div>
