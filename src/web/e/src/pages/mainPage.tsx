59a80c98644cdc53d49ff46a2665ead45a5f1094
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-29 13:50:15 -0700

    added newSurvey button + hover

--- src/web/e/src/pages/mainPage.tsx
+++ src/web/e/src/pages/mainPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -11,8 +11,33 @@ export interface mainPageProps {
 export default function mainPage() {
     const { data: session } = useSession();
     const router = useRouter();
-    
-    if (!session) {
-        router.push('/')
+    const [isHovered, setIsHovered] = useState(false);
+
+    function newSurveyButtonOnMouseEnter() {
+        setIsHovered(true);
     }
+
+    function newSurveyButtonOnMouseLeave() {
+        setIsHovered(false);
+    }
+
+    let newSurveyButtonStyle = "flex bg-black h-16 w-32 text-center text-white rounded-lg m-5 relative";
+    if (isHovered) {
+        newSurveyButtonStyle = "flex bg-black h-20 w-36 text-center text-white rounded-lg m-5 relative"
+    }
+    
+    useEffect(() => {
+        if (!session) {
+            router.push('/')
+        }
+    })
+    
+
+    return (
+        <div className="flex justify-center">
+            <div className={newSurveyButtonStyle} onMouseEnter={newSurveyButtonOnMouseEnter} onMouseLeave={newSurveyButtonOnMouseLeave}>
+                <span className="m-auto">New survey</span>
+            </div>
+        </div>
+    )
 }
