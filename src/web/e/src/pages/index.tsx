93d66ee2757d1a300e638e4c393b3395cd6d913f
Sebastian Larson <sebastianlarson22@gmail.com>
2023-07-28 18:48:22 -0700

    working on landing

--- src/web/e/src/pages/index.tsx
+++ src/web/e/src/pages/index.tsx
@@ -2,49 +2,59 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "e/utils/api";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { data: session } = useSession();
   const [isHovered, setIsHovered] = useState(false);
+  const router = useRouter();
 
-  function onMouseEnterAuthButton(event: React.MouseEvent): void {
-    setIsHovered(true)
-  }
+  if (!session) {
 
-  function onMouseLeaverAuthButton(event: React.MouseEvent): void{
-    setIsHovered(false)
-  }
+    function onMouseEnterAuthButton(event: React.MouseEvent): void {
+      setIsHovered(true)
+    }
 
-  let authButtonStyle = "flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg"
-  if (isHovered) {
-    authButtonStyle = "flex bg-gray-600 h-24 w-36 text-xl text-center rounded-lg"
-  }
-  return (
-    <div>
+    function onMouseLeaverAuthButton(event: React.MouseEvent): void{
+      setIsHovered(false)
+    }
 
-      <Head>
-        <title>e</title>
-      </Head>
-      
-      <div className="flex flex-auto flex-col">
-        <div className="flex flex-auto justify-center">
-          <h1 className="text-9xl m-11">e</h1>
-        </div>
+    let authButtonStyle = "flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg"
+    if (isHovered) {
+      authButtonStyle = "flex bg-gray-600 h-24 w-36 text-xl text-center rounded-lg"
+    }
+    return (
+      <div>
 
-        <div className="flex flex-auto justify-center">
-          <h1 className="text-5xl m-11">LLM powered surveys</h1>
-        </div>
-
-        <div className="flex flex-auto justify-center flex-row space-x-28 relative inset-y-44">
-          <div className={authButtonStyle} onMouseEnter={onMouseEnterAuthButton} onMouseLeave={onMouseLeaverAuthButton}>
-            <span className="m-auto">Get Started</span>
+        <Head>
+          <title>e</title>
+        </Head>
+        
+        <div className="flex flex-auto flex-col">
+          <div className="flex flex-auto justify-center">
+            <h1 className="text-9xl m-11">e</h1>
           </div>
-          <div className="flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg">
-            <span className="m-auto">Sign In</span>
+
+          <div className="flex flex-auto justify-center">
+            <h1 className="text-5xl m-11">LLM powered surveys</h1>
+          </div>
+
+          <div className="flex flex-auto justify-center flex-row space-x-28 relative inset-y-44">
+            <div className={authButtonStyle} onMouseEnter={onMouseEnterAuthButton} onMouseLeave={onMouseLeaverAuthButton} onClick={() => signIn()}>
+              <span className="m-auto">Get Started</span>
+            </div>
+            <div className="flex bg-gray-600 h-20 w-32 text-xl text-center rounded-lg">
+              <span className="m-auto">Sign In</span>
+            </div>
           </div>
         </div>
+
       </div>
+    );
+  }
 
-    </div>
-  );
+  else {
+    router.push('/mainPage');
+  }
 }
