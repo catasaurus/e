2681740520aa9d6c29058220223530188f7cc1b7
Sebastian Larson <sebastianlarson22@gmail.com>
2023-08-02 15:27:00 -0700

    i have no idea why surveys work part of the time (maybe vercel 10 second limit?)

--- src/web/e/src/env.mjs
+++ src/web/e/src/env.mjs
@@ -21,9 +21,9 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
-    // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+
+    DISCORD_CLIENT_ID: z.string().min(1),
+    DISCORD_CLIENT_SECRET: z.string().min(1),
 
     PALM_API_KEY: z.string().min(1),
   },
--- src/web/e/src/server/api/routers/survey.ts
+++ src/web/e/src/server/api/routers/survey.ts
@@ -14,8 +14,8 @@ export const surveyRouter = createTRPCRouter({
     // procedure is protected so session is guaranteed not to be undefined
     runSurvey: protectedProcedure
         .input(z.object({ mainTraits: z.array(z.string()), subTraits: z.array(z.string()), miniTraits: z.array(z.string()), question: z.string() }))
-        .mutation(({ ctx, input }) => {
-            void runSurvey(input.mainTraits, input.subTraits, input.miniTraits, input.question, ctx.session.user.id, ctx.prisma)
+        .mutation(async ({ ctx, input }) => {
+            void await runSurvey(input.mainTraits, input.subTraits, input.miniTraits, input.question, ctx.session.user.id, ctx.prisma);
         }),
     
     getSurveys: protectedProcedure
